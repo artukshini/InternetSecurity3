fbfdd67eff36283df17bab10543c99cb503c3ed6
Art Ukshini <94797611+artukshini@users.noreply.github.com>
2023-02-01 04:16:47 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,3 +10,12 @@ func help()
 	fmt.Println("Stresstest <URL> <COUNT>\nExample:\nStresstest https://www.testime.siguria.ne.internetPROJEKTI-III.com 1000")
 }
 
+func main() {
+
+	if len(os.Args) < 2 {
+		help()
+		os.Exit(0)
+	}
+	
+		app.MainStress(os.Args[1], os.Args[2])
+}
